f787953b3f784de0ef260c4e5b9913cbc578d993
sye0x <s.r.mahamid@gmail.com>
2026-02-10 04:58:45 +0500

    bug fix

--- server/db.js
+++ server/db.js
@@ -1,8 +1,13 @@
+require("dotenv").config();
 const { Pool } = require("pg");
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is missing");
+}
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: isProduction ? { rejectUnauthorized: false } : false,
+  ssl: { rejectUnauthorized: false },
 });
 
 module.exports = pool;
