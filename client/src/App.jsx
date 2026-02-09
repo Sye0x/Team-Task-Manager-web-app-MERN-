adf07549f619bd28ece811ccbc6256db1ee00d87
sye0x <s.r.mahamid@gmail.com>
2026-02-09 22:55:47 +0500

    Password Constraint

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import Register from "./pages/register";
 import Login from "./pages/login";
 import Dashboard from "./pages/Dashboard";
--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,10 +1,26 @@
 const bcrypt = require("bcrypt");
 const pool = require("../db");
+const validator = require("validator");
 
 exports.register = async (req, res) => {
   const { first_name, last_name, email, password } = req.body;
 
   try {
+    if (
+      !validator.isStrongPassword(password, {
+        minLength: 8,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 1,
+      })
+    ) {
+      return res.status(400).json({
+        error:
+          "Password must be at least 8 characters and include uppercase, lowercase, number and symbol.",
+      });
+    }
+
     const hashed = await bcrypt.hash(password, 10);
 
     await pool.query(
@@ -12,7 +28,10 @@ exports.register = async (req, res) => {
       [first_name, last_name, email, hashed],
     );
 
-    res.status(200).json({ success: true, message: "Registered successfully" });
+    res.status(200).json({
+      success: true,
+      message: "Registered successfully",
+    });
   } catch (err) {
     console.log(err);
     res.status(400).json({ error: err.message });
