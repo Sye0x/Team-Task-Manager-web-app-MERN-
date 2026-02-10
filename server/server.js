acde5098c1ed409eac90b1fd0d5afe31a3f49903
sye0x <s.r.mahamid@gmail.com>
2026-02-10 05:43:10 +0500

    re added listner

--- server/server.js
+++ server/server.js
@@ -39,3 +39,9 @@ app.use("/tasks", taskRoutes);
 app.get("/", (req, res) => {
   res.json({ message: "Hello World" });
 });
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
